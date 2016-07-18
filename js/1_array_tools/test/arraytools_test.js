568ed6cab76a25d883917965e9c0d718c5c42a1b
Uladzislau Klimiankou <vlad.klimenkoff97@gmail.com>
2016-07-18 14:01:08 +0300

    Implement task 1 - array tools TODO html page TODO add O(n) max subsum

---
+++ js/1_array_tools/test/arraytools_test.js
@@ -0,0 +1,49 @@
+var assert = require('chai').assert;
+var ArrayTools = require('../script');
+
+describe('ArrayTools', () => {
+    describe('#getMaxSubSum2()', () => {
+        it('should be correct', () => {
+            assert.equal(6, ArrayTools.getMaxSubSum2([1, 2, 3]));
+            assert.equal(5, ArrayTools.getMaxSubSum2([-1, 2, 3, -9]));
+            assert.equal(6, ArrayTools.getMaxSubSum2([2, -1, 2, 3, -9]));
+            assert.equal(100, ArrayTools.getMaxSubSum2([100, -9, 2, -3, 5]));
+        });
+    });
+});
+
+// describe('ArrayTools', () => {
+//     describe('#getMaxSubSum()', () => {
+//         it('should be correct', () => {
+//             assert.equal(6, ArrayTools.getMaxSubSum([1, 2, 3]));
+//             assert.equal(5,ArrayTools. getMaxSubSum([-1, 2, 3, -9]));
+//             assert.equal(6, ArrayTools.getMaxSubSum([2, -1, 2, 3, -9]));
+//             assert.equal(100, ArrayTools.getMaxSubSum([100, -9, 2, -3, 5]));
+//         });
+//     });
+// });
+
+describe('ArrayTools', () => {
+   describe('#findMin()', () => {
+       it('should return min element', () => {
+           assert.equal(-40, ArrayTools.findMin([7, -7, 13, 39, 1, -40, 8]));
+       });
+   });
+});
+
+describe('ArrayTools', () => {
+    describe('#findMax()', () => {
+        it('should return max element', () => {
+            assert.equal(39, ArrayTools.findMax([7, -7, 13, 39, 1, -40, 8]));
+        });
+    });
+});
+
+describe('ArrayTools', () => {
+    describe('#findMedian()', () => {
+        it('should return median element', () => {
+            assert.equal(39, ArrayTools.findMedian([7, -7, 13, 39, 1, -40, 8]));
+            assert.equal(20, ArrayTools.findMedian([7, -7, 13, 39, 1, -40, 8, 5]));
+        });
+    });
+});
