c0e94d05790f0d68cd07851717bfc051606267e8
Uladzislau Klimiankou <vlad.klimenkoff97@gmail.com>
2016-07-20 12:40:49 +0300

    Implement working json viewer with expandable nodes

--- js/json_viewer/script.js
+++ js/json_viewer/script.js
@@ -13,24 +13,22 @@
         }
 
         var div = document.createElement('div');
-        var divHead = document.createElement('div');
-        divHead.classList.add('nodeHead');
-
         div.classList.add('expandable');
 
-        var divChilds = document.createElement('div');
-        divChilds.classList.add('childs');
-
+        var divChildren = document.createElement('div');
+        var type;
         var child;
         if (Array.isArray(obj)) {
             for (var i = 0; i < obj.length; i++) {
                 child = obj[i];
                 if (typeof child === 'object') {
-                    divChilds.appendChild(buildJsonTree(obj[i], i));
+                    divChildren.appendChild(buildJsonTree(obj[i], i));
                 } else {
-                    divChilds.appendChild(buildPrimitiveView(i, obj[i]));
+                    divChildren.appendChild(buildPrimitiveView(i, obj[i]));
                 }
             }
+
+            type = 'array';
         } else {
             for (var prop in obj) {
                 if (!obj.hasOwnProperty(prop)) {
@@ -39,24 +37,24 @@
 
                 child = obj[prop];
                 if (typeof child === 'object') {
-                    divChilds.appendChild(buildJsonTree(obj[prop], prop));
+                    divChildren.appendChild(buildJsonTree(obj[prop], prop));
                 } else {
-                    divChilds.appendChild(buildPrimitiveView(prop, obj[prop]));
+                    divChildren.appendChild(buildPrimitiveView(prop, obj[prop]));
                 }
             }
+
+            type = 'object';
         }
 
-        divHead.innerHTML = key;
+        divChildren.classList.add('children');
 
-        div.appendChild(divHead);
-        div.appendChild(divChilds);
+        div.appendChild(buildHeadView(key, type));
+        div.appendChild(divChildren);
 
         return div;
     }
 
     function buildPrimitiveView(key, value) {
-        var div = document.createElement('div');
-
         var valueType;
         if (value === null) {
             valueType = 'null';
@@ -64,19 +62,36 @@
             valueType = typeof value;
         }
 
+        return buildHeadView(key + ": " + value, valueType);
+    }
+
+    function buildHeadView(content, type) {
+        var div = document.createElement('div');
+        div.classList.add('clearfix', 'node-head');
+
         var divIndicator = document.createElement('div');
-        divIndicator.classList.add('type-indicator', valueType);
+        divIndicator.classList.add('type-indicator', type);
 
         var divContent = document.createElement('div');
-        divContent.innerHTML = key + ": " + value;
+        divContent.classList.add('content');
+        divContent.innerHTML = content;
+
+        if (type === 'object' || type === 'array') {
+            var spoiler = document.createElement('span');
+            spoiler.classList.add('spoiler');
+            div.appendChild(spoiler);
+        }
 
         div.appendChild(divIndicator);
         div.appendChild(divContent);
         return div;
     }
 
+    // Bind listeners
     document.getElementById('view-btn').onclick = function() {
         var jsonText = document.getElementById('json-text').value;
+        var jsonObjElement = document.getElementById('json-obj');
+        jsonObjElement.innerHTML = "";
 
         var obj;
         try {
@@ -84,12 +99,27 @@
         } catch (e) {
             if (e instanceof SyntaxError) {
                 drawInvalidJson();
+                return;
             }
         }
 
         var objTree = buildJsonTree(obj);
-        document.getElementById('json-obj').appendChild(objTree);
+        jsonObjElement.appendChild(objTree);
 
+        var expandables = document.getElementsByClassName('expandable');
+        var elem;
+        for (var i = 0; i < expandables.length; i++) {
+            elem = expandables[i];
+            (function(elem) {
+                elem.getElementsByClassName('spoiler')[0].onclick = function () {
+                    if (elem.classList.contains('minimized')) {
+                        elem.classList.remove('minimized');
+                    } else {
+                        elem.classList.add('minimized');
+                    }
+                }
+            })(elem);
+        }
     };
 })();
 
