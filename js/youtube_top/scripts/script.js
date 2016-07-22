f395bf7083b1e9df765f654a1bb1bbfe7f2484f6
Uladzislau Klimiankou <vlad.klimenkoff97@gmail.com>
2016-07-22 12:13:26 +0300

    Slider moved to independent class

--- js/youtube_top/scripts/script.js
+++ js/youtube_top/scripts/script.js
@@ -1,100 +1,129 @@
 (function () {
     var yservice = new YoutubeService();
-    var text = '';
-    yservice.getTopViewed(10, buildSlider);
+    yservice.getTopViewed(10, init);
 
-    function buildSlider(previews) {
-        var slider = document.querySelector('.slider'),
-            previewDivs = [],
-            previewDiv,
-            img,
-            link,
-            title,
-            i;
-
-        for (i = 0; i < previews.length; i++) {
-            previewDiv = createDiv('preview');
-
-            img = document.createElement('img');
-            img.src = previews[i].img;
-
-            link = createLink(previews[i].link, img);
-            title = createDiv('title');
-            title.innerHTML = previews[i].title;
-
-            previewDiv.appendChild(link);
-            previewDiv.appendChild(title);
-
-            slider.appendChild(previewDiv);
-        }
-
-        // Bind listeners to slider
-        slider.onmousedown = function (e) {
-            e.preventDefault();
-
-            slider.onclick = function () {}
-            window.onmousemove = sliderMove;
-        };
-
-        window.onmouseup = function (e) {
-            console.log(e);
-            sliderRelease(e);
-            window.onmousemove = function () {};
-        };
+    function init (previews) {
+        var slider = new Slider();
+        slider.buildSlider(previews);
+        slider.insertSlider(document.querySelector('.slider-container'));
     }
 
-    function sliderMove(e) {
-        document.querySelector('.slider').onclick = function(e) {
-            e.preventDefault();
-        };
+    function Slider() {
+        var slider,
+            previews,
+            previewFullWidth,
+            sliderVisibleWidth,
+            sliderFullWidth;
 
-        var slider = document.querySelector('.slider');
-        var sliderOffsetLeft = slider.offsetLeft + e.movementX;
-        slider.style.left = sliderOffsetLeft + 'px';
-    }
+        function calcSizes() {
+            sliderVisibleWidth = parseInt(getComputedStyle(slider).width);
 
-    function sliderRelease(e) {
-        var slider = document.querySelector('.slider'),
-            leftOffset = parseInt(slider.style.left || getComputedStyle(slider).left),
-            width = parseInt(getComputedStyle(slider).width);
+            var previewWidth = parseInt(getComputedStyle(previews[0]).width);
+            var previewIndent = parseInt(getComputedStyle(previews[0]).marginLeft) +
+                parseInt(getComputedStyle(previews[0]).marginRight);
 
-        var previews = slider.children;
-        var previewWidth = parseInt(getComputedStyle(previews[0]).width);
-        var previewIndent = parseInt(getComputedStyle(previews[0]).marginLeft) +
-            parseInt(getComputedStyle(previews[0]).marginRight);
-        var fullWidth = (previewWidth + previewIndent) * previews.length;
-        var rightLimit = width - fullWidth;
-
-        var interval;
-        if (leftOffset > 0) {
-            interval = setInterval(movingLeft, 1);
-        } else if (leftOffset < rightLimit) {
-            interval = setInterval(movingRight, 1);
+            previewFullWidth = previewWidth + previewIndent;
+            sliderFullWidth = previewFullWidth * previews.length;
         }
 
-        function movingLeft () {
-            if (leftOffset <= 0) {
-                leftOffset = 0;
-                slider.style.left = leftOffset + 'px';
-                clearInterval(interval)
-            } else {
-                leftOffset -= 10;
-                slider.style.left = leftOffset + 'px';
+        this.insertSlider = function(parent) {
+            parent.appendChild(slider);
+            calcSizes();
+        };
+
+        this.buildSlider = function(previewsData) {
+            var preview,
+                img,
+                link,
+                title,
+                i;
+
+            slider = createDiv('slider');
+
+            for (i = 0; i < previewsData.length; i++) {
+                preview = createDiv('preview');
+
+                img = document.createElement('img');
+                img.src = previewsData[i].img;
+
+                link = createLink(previewsData[i].link, img);
+                title = createDiv('title');
+                title.innerHTML = previewsData[i].title;
+
+                preview.appendChild(link);
+                preview.appendChild(title);
+
+                slider.appendChild(preview);
+            }
+
+            previews = slider.children;
+
+            // Bind listeners to slider
+            slider.onmousedown = function (e) {
+                e.preventDefault();
+                slider.onclick = function () {};
+                window.onmousemove = sliderMove;
+            };
+
+            window.onmouseup = function (e) {
+                console.log(e);
+                sliderRelease(e);
+                window.onmousemove = function () {};
+            };
+        };
+
+        function sliderMove(e) {
+            slider.onclick = function(e) {
+                e.preventDefault();
+            };
+
+            var sliderOffsetLeft = slider.offsetLeft + e.movementX;
+            slider.style.left = sliderOffsetLeft + 'px';
+        }
+
+        function sliderRelease(e) {
+            var leftOffset = parseInt(slider.style.left || getComputedStyle(slider).left);
+            var rightLimit = sliderVisibleWidth - sliderFullWidth;
+
+            var interval;
+            if (leftOffset > 0) {
+                interval = setInterval(movingLeft, 1);
+            } else if (leftOffset < rightLimit) {
+                interval = setInterval(movingRight, 1);
+            }
+
+            function movingLeft () {
+                if (leftOffset <= 0) {
+                    leftOffset = 0;
+                    slider.style.left = leftOffset + 'px';
+                    clearInterval(interval)
+                } else {
+                    leftOffset -= 10;
+                    slider.style.left = leftOffset + 'px';
+                }
+            }
+
+            function movingRight () {
+                if (leftOffset >= rightLimit) {
+                    leftOffset = rightLimit;
+                    slider.style.left = leftOffset + 'px';
+                    clearInterval(interval)
+                } else {
+                    leftOffset += 10;
+                    slider.style.left = leftOffset + 'px';
+                }
             }
         }
 
-        function movingRight () {
-            debugger
-            if (leftOffset >= rightLimit) {
-                leftOffset = rightLimit;
-                slider.style.left = leftOffset + 'px';
-                clearInterval(interval)
-            } else {
-                leftOffset += 10;
-                slider.style.left = leftOffset + 'px';
-            }
+        // Scrolls slider to index-s preview
+        function scrollTo(index, slider, leftOffset) {
+
+
+
         }
     }
+
+
 
     function createLink(href, inner) {
         var link = document.createElement('a');
