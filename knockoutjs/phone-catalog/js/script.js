81a391a034bddbe7cffbfd8adc4835899e53f3aa
Uladzislau Klimiankou <vlad.klimenkoff97@gmail.com>
2016-08-01 17:45:12 +0300

    Add displaying phone list

--- knockoutjs/phone-catalog/js/script.js
+++ knockoutjs/phone-catalog/js/script.js
@@ -3,5 +3,30 @@ fetch('/data/phones-info.json')
         return res.json();
     })
     .then(data => {
-        console.log(data);
+        initCatalog(data);
     });
+
+let shoppingCart = {
+    count: 0,
+    phones: ko.observableArray(),
+    totalPrice: 0
+};
+
+let phone = {
+    name: ko.observable(),
+    infoLink: ko.observable(),
+    imgLink: ko.observable(),
+    description: ko.observable(),
+    price: ko.observable()
+};
+
+let viewModel = {
+    phones: ko.observableArray(),
+    shoppingCart: shoppingCart
+};
+
+
+function initCatalog(data) {
+    viewModel.phones = ko.observableArray(data);
+    ko.applyBindings(viewModel);
+}
