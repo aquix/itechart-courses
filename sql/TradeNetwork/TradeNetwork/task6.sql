a8f469b08b26f289dcbf5767f59f40a2039b4cd5
Uladzislau Klimiankou <vlad.klimenkoff97@gmail.com>
2016-08-16 12:50:25 +0300

    Implement 6 sql task

--- sql/TradeNetwork/TradeNetwork/task6.sql
+++ sql/TradeNetwork/TradeNetwork/task6.sql
@@ -1,27 +1,20 @@
-﻿create function calc_count_of_sales_in_interval(@interval_start nvarchar(10), @interval_end nvarchar(10))
-returns @ret table
-	(id int primary key,
-	 count int)
-as
-begin
-	declare @sales_table table (id int);
-	set @sales_table = (select products.id
+﻿select products.id, products.name, count_in_2014, isnull(count_in_2015, 0) as count_in_2015
+from (select products.id, count(*) as count_in_2014
 		from orders_products
 		join orders
 		on orders_products.order_id = orders.id
 		join products
 		on orders_products.product_id = products.id
-		where orders.date >= @interval_start and orders.date < @interval_end);
-
-	select @ret = (select id, count(*) as count
-		from @sales_table
-		group by id)
-	return
-end;
-
-select *
-from calc_count_of_sales_in_interval('2015-01-01', '2015-12-31') as sales2015
-join calc_count_of_sales_in_interval('2014-01-01', '2014-12-31') as sales2014
+		where orders.date >= '2014-01-01' and orders.date < '2014-12-31' group by products.id) as sales2014
+left join (select products.id, count(*) as count_in_2015
+		from orders_products
+		join orders
+		on orders_products.order_id = orders.id
+		join products
+		on orders_products.product_id = products.id
+		where orders.date >= '2015-01-01' and orders.date < '2015-12-31' group by products.id) as sales2015
 on sales2015.id = sales2014.id
-
--- TODO
+join products
+on sales2014.id = products.id
+where (count_in_2015 is null) or (count_in_2014 > count_in_2015)
+order by products.name
