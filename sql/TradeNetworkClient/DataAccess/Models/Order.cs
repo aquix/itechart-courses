bc62ceee5186297d4d0cd6901a58966ee2e9592c
Uladzislau Klimiankou <vlad.klimenkoff97@gmail.com>
2016-08-17 13:28:43 +0300

    TradeNetworkClient models

---
+++ sql/TradeNetworkClient/DataAccess/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Price { get; set; }
+        public int CustomerId { get; set; }
+        public int ShopId { get; set; }
+
+        public Customer Customer { get; set; }
+        public Shop Shop { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ sql/TradeNetworkClient/DataAccess/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public List<Shop> Shops { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
